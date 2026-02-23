43e114205d17459f1728f81ac5e4b110ce06e508
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-02-23 10:56:33 -0300

    Replace APIView with ModelViewSet for products

--- products/views.py
+++ products/views.py
@@ -1,11 +1,15 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from products.models import Product, Order
+from products.serializers import ProductSerializer, OrderSerializer
 
-class ViewProtegida(APIView):
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # Opcional: define permissão específica por view
+    permission_classes = [IsAuthenticated] 
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        return Response({
-            "mensagem": "Você está autenticado com sucesso!"
-        })
