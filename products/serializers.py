cf04232cb1c08084b661a50144780b2e832ef5ef
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-04-10 02:32:10 +0000

    3

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Produto
+from .models import Produto, Order
 
 class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Produto
         fields = ["id", "nome", "preco", "descricao"]
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ["id", "produto", "quantidade", "data_pedido"]
